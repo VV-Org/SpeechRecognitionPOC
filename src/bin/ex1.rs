cc861edf47ae4d3bfee6281019eb433b272cb6d3
guilhermo <guilhermo.readonly@gmail.com>
2020-12-25 20:42:15 +0100

    cost function

--- src/bin/ex1.rs
+++ src/bin/ex1.rs
@@ -1,3 +1,11 @@
-fn main(){
-    println!("Exo 1.");
+use nalgebra::VectorN;
+use speech_recognition_poc::cost_with_hypothesis;
+
+fn main() {
+    
+    let outputs_actual = VectorN::from(vec![1.0, 4.0, 4.0, 9.0, 16.0]);
+    let inputs = VectorN::from(vec![1.0, 2.0, 2.0, 3.0, 4.0]);
+
+    let test = cost_with_hypothesis(&inputs, &outputs_actual, &|x|x*x);
+    println!("Test = {}", test);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,56 @@
+extern crate nalgebra as na;
+
+use na::{Dynamic, VectorN};
+
+pub fn cost_with_hypothesis(
+    inputs: &VectorN<f64, Dynamic>,
+    outputs_actual: &VectorN<f64, Dynamic>,
+    hypothesis: &dyn Fn(f64) -> f64,
+) -> f64 {
+    let outputs_predicted: VectorN<f64, Dynamic> = inputs.map(hypothesis);
+    cost(outputs_actual, &outputs_predicted)
+}
+
+pub fn cost(
+    outputs_actual: &VectorN<f64, Dynamic>,
+    outputs_predicted: &VectorN<f64, Dynamic>,
+) -> f64 {
+    dbg!(&outputs_actual);
+    let diff = outputs_actual - outputs_predicted;
+
+    dbg!(&diff);
+    let diff_squared = &diff.component_mul(&diff) ;
+
+    dbg!(&diff_squared);
+
+    let diff_squared_mean = VectorN::mean(&diff_squared);
+
+    dbg!(&diff_squared_mean);
+
+    diff_squared_mean / 2.0
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::cost;
+    use nalgebra::VectorN;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_cost_with_outputs() {
+        let outputs_actual = VectorN::from(vec![0.0, 0.0, 0.0, 0.0, 0.0]);
+        let outputs_predicted = VectorN::from(vec![0.0, 0.0, 0.0, 0.0, 0.0]);
+        assert_eq!(0.0, cost(&outputs_actual, &outputs_predicted));
+
+        let outputs_actual = VectorN::from(vec![2.0, 2.0, 2.0, 2.0]);
+        let outputs_predicted = VectorN::from(vec![2.0, 2.0, 2.0, 2.0]);
+        assert_eq!(0.0, cost(&outputs_actual, &outputs_predicted));
+
+        let outputs_actual = VectorN::from(vec![2.0, 2.0, 2.0, 2.0, 0.0]);
+        let outputs_predicted = VectorN::from(vec![2.0, 2.0, 2.0, 2.0, 2.0]);
+        assert_eq!(0.4, cost(&outputs_actual, &outputs_predicted));
+
+        let outputs_actual = VectorN::from(vec![2.0, 2.0, 4.0, 2.0, 2.0]);
+        let outputs_predicted = VectorN::from(vec![2.0, 2.0, 2.0, 2.0, 2.0]);
+        assert_eq!(0.4, cost(&outputs_actual, &outputs_predicted));
     }
 }
